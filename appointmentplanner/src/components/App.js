20ea6e253e6199e0f82d9500292bbda290af28cf
Blancher <108644584+Blancher@users.noreply.github.com>
2023-02-27 16:41:04 -0800

    Update App.js

--- appointmentplanner/src/components/App.js
+++ appointmentplanner/src/components/App.js
@@ -1,12 +1,16 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import NavBar from './NavBar';
 import Contacts from './Contacts';
 import Appointments from './Appointments';
 
 export default function App() {
-  const [contacts, setContacts] = useState([]);
-  const [appointments, setAppointments] = useState([]);
+  const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem("contacts")) || []);
+  const [appointments, setAppointments] = useState(JSON.parse(localStorage.getItem("appointments")) || []);
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+    localStorage.setItem("appointments", JSON.stringify(appointments));
+  });
   return (
     <Router id='router'>
       <header>
